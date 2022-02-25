286a66310bcb330ecee89453007323b197d8b109
Romenick Tester <romenick.tester@gmail.com>
2022-02-25 11:31:35 +0000

    client: landing page styled-components exercise

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -11,7 +11,7 @@ const LandingPage = () => {
             </nav>
             <div className="container page">
                 <div className="info">
-                    <h1>job tracking app</h1>
+                    <h1>job <span>tracking</span> app</h1>
                     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores dolore neque inventore iste veniam, mollitia, qui doloremque provident impedit nisi nemo nulla quod! At natus in laudantium recusandae, fugiat ipsum?</p>
                     <button className="btn btn-hero">login/register</button>
                 </div>
@@ -22,7 +22,48 @@ const LandingPage = () => {
 };
 
 const Wrapper = styled.main`
+    nav {
+        width: var(--fluid-width);
+        max-width: var(--max-width);
+        margin: 0 auto;
+        height: var(--nav-height);
+        display: flex;
+        align-items: center;
+    }
 
+    .page {
+        min-height: calc(100vh - var(--nav-height));
+        display: grid;
+        align-items: center;
+        margin-top: -3rem;
+    }
+
+    h1 {
+        font-weight: 700;
+
+        span {
+            color: var(--primary-500);
+        }
+    }
+
+    p {
+        color: var(--grey-600);
+    }
+
+    .main-img {
+        display: none;
+    }
+
+    @media (min-width: 992px) {
+        .page {
+            grid-template-columns: 1fr 1fr;
+            column-gap: 3rem;
+        }
+
+        .main-img {
+            display: block;
+        }
+    }
 `
 
 export default LandingPage;
